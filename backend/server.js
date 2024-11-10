ba6ed248f4581facf87f0d278522413fbee7a5f3
Parth Patadia <parthpatadia78@gmail.com>
2024-11-10 19:32:44 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
 
-// Connect to database
 connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://to-do-app-two-sigma.vercel.app/login'
+    ],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
 app.use('/api/todos', require('./routes/todoRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
-// Error handler
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-}); 
+});
+
+module.exports = app; 
